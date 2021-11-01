ab235724bb7ebd9be5fb7eeb9176e6d2b65047f2
Joakim Jäderberg <bananashell@gmail.com>
2021-11-01 10:05:19 +0100

    Added GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,40 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Script from "next/script";
+import { FunctionComponent } from "react";
 
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={theme}>
-            <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
-                <ReactQueryDevtools />
-            </QueryClientProvider>
-        </ThemeProvider>
+        <>
+            <GoogleAnalytics />
+            <ThemeProvider theme={theme}>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools />
+                </QueryClientProvider>
+            </ThemeProvider>
+        </>
     );
 }
 
 export default MyApp;
+
+const GoogleAnalytics: FunctionComponent = () => (
+    <>
+        <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+    </>
+);
